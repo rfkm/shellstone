f41841846f59bfa63f9e70adb214cf980f8684fb
Ryo Fukumuro <rkworks@o0o.co>
2014-12-05 18:04:29 +0900

    Fix CI

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject shellstone "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "An experimental programming language in which Method Shells is implemented"
+  :url "https://github.com/rkworks/shellstone"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
@@ -8,4 +8,5 @@
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.blancas/kern "0.7.0"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]
-                                  [org.clojure/tools.trace "0.7.8"]]}})
+                                  [org.clojure/tools.trace "0.7.8"]]
+                   :plugins [[lein-midje "3.1.1"]]}})
